7e092d4824808c97a85d0c8d511415f7f8744421
Charlotte Mannion <charlottemannion100@gmail.com>
2018-11-28 11:42:17 +0000

    creates a selectBreakfastItem function that returns

--- jasmine-standalone-3.2.1/spec/menuSpec.js
+++ jasmine-standalone-3.2.1/spec/menuSpec.js
@@ -4,7 +4,15 @@ describe("Menu", function() {
         menu = new Menu();
     })
 
-    it("has breakfast items", function() {
-        expect(menu.breakfastOptions).toEqual(["weetabix", "corn flakes", "rice krispies", "bran flakes", "muesli"])
+    describe("items on the menu", function() {
+        it("has breakfast items", function() {
+            expect(menu.breakfastOptions).toEqual(["weetabix", "corn flakes", "rice krispies", "bran flakes", "muesli"])
+        })
+    })
+
+    describe("the function to choose breakfast", function() {
+        it("selects a breakfast item", function() {
+            expect(menu.selectBreakfastItem(1)).toEqual("corn flakes")
+        })
     })
 })
--- jasmine-standalone-3.2.1/src/Menu.js
+++ jasmine-standalone-3.2.1/src/Menu.js
@@ -1,3 +1,7 @@
 function Menu() {
     this.breakfastOptions = ["weetabix", "corn flakes", "rice krispies", "bran flakes", "muesli"]
 }
+
+Menu.prototype.selectBreakfastItem = function() {
+    return "corn flakes"
+}
