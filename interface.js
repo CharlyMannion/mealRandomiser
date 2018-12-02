5a68d31db1963c15fd2265644732e312bd1d4a35
Charlotte Mannion <charlottemannion100@gmail.com>
2018-12-02 11:37:01 +0000

    adds more buttons to the index page

--- interface.js
+++ interface.js
@@ -1,7 +1,5 @@
 $( document ).ready(function() {
     $("button").click(function(){
-        $("#viewMenu").hide();
-        $("#mealRandomiserHeading").hide();
-        $("#randomiseChoice").hide();
+        $("#randomCereal").hide();
     });
 });
