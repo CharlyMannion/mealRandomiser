1725b6cb7dba9f7a48f608b357ab301092c9c7be
Charlotte Mannion <charlottemannion100@gmail.com>
2018-12-03 10:51:54 +0000

    connects button elements to menu model

--- interface.js
+++ interface.js
@@ -4,8 +4,32 @@ $( document ).ready(function() {
     $("#cerealChoice").hide();
     $('#cerealChoice').text(menu.selectBreakfastItem());
 
+    $("#toastChoice").hide();
+    $('#toastChoice').text(menu.selectToast());
+
+    $("#coldPuddingChoice").hide();
+    $('#coldPuddingChoice').text(menu.selectColdPudding());
+
+    $("#snackChoice").hide();
+    $('#snackChoice').text(menu.selectSnack());
+
     $("#randomCereal").on('click', (function(){
         $("#randomCereal").hide();
         $("#cerealChoice").show();
+    }));
+
+    $("#randomToast").on('click', (function(){
+        $("#randomToast").hide();
+        $("#toastChoice").show();
+    }));
+
+    $("#randomColdPudding").on('click', (function(){
+        $("#randomColdPudding").hide();
+        $("#coldPuddingChoice").show();
+    }));
+
+    $("#randomSnack").on('click', (function(){
+        $("#randomSnack").hide();
+        $("#snackChoice").show();
     }));
 });
