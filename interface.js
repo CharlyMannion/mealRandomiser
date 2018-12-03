3f6ffb82246b934744b2504c9ccff999dfcf2e5b
Charlotte Mannion <charlottemannion100@gmail.com>
2018-12-03 10:07:30 +0000

    connects button click to menu function

--- interface.js
+++ interface.js
@@ -1,5 +1,9 @@
 $( document ).ready(function() {
+    var menu = new Menu();
+
     $("#choice").hide();
+    $('#choice').text(menu.selectBreakfastItem());
+    
     $("button").click(function(){
         $("#randomCereal").hide();
         $("#choice").show();
